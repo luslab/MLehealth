99010061dfb1ae4a9367db92b056f7392da98eea
Andrew Steele <github@andrewsteele.co.uk>
2017-02-03 17:09:14 +0000

    Finishing off first draft of replicate with missing
    (C-indices are 1 - x at the moment for some reason! Probably the Cox model used here returns inverse values for predict)

--- caliber-replicate/caliber-replicate-with-missing.R
+++ caliber-replicate/caliber-replicate-with-missing.R
@@ -1,5 +1,5 @@
 data.filename <- '../../data/cohort-sanitised.csv'
-n.data <- 1000 # This is of full dataset...further rows may be excluded in prep
+n.data <- 10000 # This is of full dataset...further rows may be excluded in prep
 
 continuous.vars <-
   c(
@@ -13,6 +13,16 @@ source('../random-forest/shared.R')
 # prepData simpler
 COHORT.full <- data.frame(fread(data.filename))
 
+# Remove the patients we shouldn't include
+COHORT.full <-
+  COHORT.full[
+    # remove negative times to death
+    COHORT.full$time_death > 0 &
+      # remove patients who should be excluded
+      !COHORT.full$exclude
+    ,
+    ]
+
 # If n.data was specified...
 if(!is.na(n.data)){
   # Take a subset n.data in size
@@ -23,6 +33,11 @@ if(!is.na(n.data)){
   COHORT.use <- COHORT.full
   rm(COHORT.full)
 }
+
+# Redefine n.data just in case we lost any rows
+n.data <- nrow(COHORT.use)
+# Define indices of test set
+test.set <- sample(1:n.data, (1/3)*n.data)
 
 # add rescaled columns to match fit with the paper, and put baseline values at
 # the start of factors
@@ -47,9 +62,9 @@ ggplot(df, aes(x,y)) +
 COHORT.scaled <-
   data.frame(
     ## Time to event
-    
+    time_death = COHORT.use$time_death,
     ## Death/censorship
-    
+    surv_event = COHORT.use$endpoint_death == 'Death',
     ## Rescaled age
     age_rescale = sapply(COHORT.use$age, ageSpline),
     ## Gender
@@ -115,6 +130,13 @@ COHORT.scaled <-
 
 # Deal with missing values...go through all the columns
 missing.suffix <- '_missing'
+missing.cols <-
+  c(
+    'diagnosis',
+    "most_deprived", "smokstatus", "total_chol_6mo_rescale", "hdl_6mo_rescale",
+    "pulse_6mo_rescale", "crea_6mo_rescale", "total_wbc_6mo_rescale",
+    "haemoglobin_6mo_rescale"
+  )
 for(surv.col in names(COHORT.scaled)) {
   # ...and, if it contains any missing values
   if(sum(is.na(COHORT.scaled[, surv.col])) > 0) {
@@ -128,36 +150,40 @@ for(surv.col in names(COHORT.scaled)) {
       # If it's a factor, NAs can be their own level
       COHORT.scaled[, surv.col] <-
         factorNAfix(COHORT.scaled[, surv.col], NAval = 'missing')
+    } else if(is.logical(COHORT.scaled[, surv.col])) {
+      # Set the NA values to the baseline so they don't contribute to the model
+      COHORT.scaled[is.na(COHORT.scaled[, surv.col]), surv.col] <- FALSE
     } else {
       # Set the NA values to the baseline so they don't contribute to the model
       COHORT.scaled[is.na(COHORT.scaled[, surv.col]), surv.col] <- 0
     }
-    
   }
 }
 
 # make a survival object
-COHORT.surv <- Surv(
-  time  = COHORT.use[, 'time_death'],
-  event = COHORT.use[, 'surv_event']
+COHORT.surv.train <- Surv(
+  time  = COHORT.scaled[-test.set, 'time_death'],
+  event = COHORT.scaled[-test.set, 'surv_event']
 )
 
 fit.exp <- survreg(
-  COHORT.survival ~
-    ### Sociodemographic characteristics ###########################################
-  ## Age in men, per year
-  ## Age in women, per year
-  ## Women vs. men
-  # ie include interaction between age and gender!
-  age_rescale*gender +
+  COHORT.surv.train ~
+    ### Sociodemographic characteristics #######################################
+    ## Age in men, per year
+    ## Age in women, per year
+    ## Women vs. men
+    # ie include interaction between age and gender!
+    age_rescale*gender +
     ## Most deprived quintile, yes vs. no
     most_deprived +
-    ### SCAD diagnosis and severity ################################################
-  ## Other CHD vs. stable angina
-  ## Unstable angina vs. stable angina
-  ## NSTEMI vs. stable angina
-  ## STEMI vs. stable angina
-  diagnosis +
+    most_deprived_missing +
+    ### SCAD diagnosis and severity ############################################
+    ## Other CHD vs. stable angina
+    ## Unstable angina vs. stable angina
+    ## NSTEMI vs. stable angina
+    ## STEMI vs. stable angina
+    diagnosis +
+    #diagnosis_missing +
     ## PCI in last 6 months, yes vs. no
     pci_6mo +
     ## CABG in last 6 months, yes vs. no
@@ -166,51 +192,57 @@ fit.exp <- survreg(
     hx_mi +
     ## Use of nitrates, yes vs. no
     long_nitrate +
-    ### CVD risk factors ###########################################################
-  ## Ex-smoker vs. never
-  ## Current smoker vs. never
-  smokstatus +
+    ### CVD risk factors #######################################################
+    ## Ex-smoker / current smoker vs. never
+    smokstatus +
+    smokstatus_missing +
     ## Hypertension, present vs. absent
     hypertension +
     ## Diabetes mellitus, present vs. absent
     diabetes_logical +
     ## Total cholesterol, per 1 mmol/L increase
     total_chol_6mo_rescale +
+    total_chol_6mo_rescale_missing +
     ## HDL, per 0.5 mmol/L increase
     hdl_6mo_rescale +
-    ### CVD co-morbidities #########################################################
-  ## Heart failure, present vs. absent
-  heart_failure +
+    hdl_6mo_rescale_missing +
+    ### CVD co-morbidities #####################################################
+    ## Heart failure, present vs. absent
+    heart_failure +
     ## Peripheral arterial disease, present vs. absent
     pad +
     ## Atrial fibrillation, present vs. absent
     hx_af +
     ## Stroke, present vs. absent
     hx_stroke +
-    ### Non-CVD comorbidities ####################################################
-  ## Chronic kidney disease, present vs. absent
-  hx_renal +
+    ### Non-CVD comorbidities ##################################################
+    ## Chronic kidney disease, present vs. absent
+    hx_renal +
     ## Chronic obstructive pulmonary disease, present vs. absent
     hx_copd +
     ## Cancer, present vs. absent
     hx_cancer +
     ## Chronic liver disease, present vs. absent
     hx_liver +
-    ### Psychosocial characteristics #############################################
-  ## Depression at diagnosis, present vs. absent
-  hx_depression +
+    ### Psychosocial characteristics ###########################################
+    ## Depression at diagnosis, present vs. absent
+    hx_depression +
     ## Anxiety at diagnosis, present vs. absent
     hx_anxiety +
-    ### Biomarkers ###############################################################
-  ## Heart rate, per 10 b.p.m increase
-  pulse_6mo_rescale +
+    ### Biomarkers #############################################################
+    ## Heart rate, per 10 b.p.m increase
+    pulse_6mo_rescale +
+    pulse_6mo_rescale_missing +
     ## Creatinine, per 30 μmol/L increase
     crea_6mo_rescale +
+    crea_6mo_rescale_missing +
     ## White cell count, per 1.5 109/L increase
     total_wbc_6mo_rescale +
+    total_wbc_6mo_rescale_missing +
     ## Haemoglobin, per 1.5 g/dL increase
-    haemoglobin_6mo_rescale,
-  data = COHORT,
+    haemoglobin_6mo_rescale +
+    haemoglobin_6mo_rescale_missing,
+  data = COHORT.scaled[-test.set, ],
   dist = "exponential"
 )
 
@@ -219,25 +251,29 @@ coefficients <- -fit.exp$coeff
 cat(
   'Covariate                theirs         ours
   Age if man, years	        0.063230409    ', coefficients['age_rescale'],'
-  Age if woman, years	      0.078075876    ', coefficients['age_rescale:gender2'],'
-  Being a woman             -0.54888796    ', coefficients['gender2'],'
-  Most deprived quintile    0.140887623    ', coefficients['most_deprivedTRUE'],'
+  Age if woman, years	      0.078075876    ', coefficients['age_rescale:genderWomen'],'
+  Being a woman             -0.54888796    ', coefficients['genderWomen'],'
+  DEPRIVATION: bottom 5th   0.140887623    ', coefficients['most_deprivedTRUE'],'
+  DEPRIVATION: missing                     ', coefficients['most_deprived_missingTRUE'],'
   CAD DIAGNOSIS & SEVERITY
   Stable_angina (ref)	      0.03           ', 'hmm - how does ref have a beta?','
   Unstable angina	          0.023          ', coefficients['diagnosisUA'],'
   STEMI	                    0.079863191    ', coefficients['diagnosisSTEMI'],'
   NSTEMI                    0.260971786    ', coefficients['diagnosisNSTEMI'],'
-  PCI last 6 months	        -0.429038846   ', coefficients['pci_6mo'],'
-  CABG last 6 months	      -0.661443369   ', coefficients['cabg_6mo'],'
-  Previous/recurrent MI	    0.128214367    ', coefficients['age_rescale'],'
-  Nitrates                  0.142262066    ', coefficients['long_nitrate'],'
+  PCI last 6 months	        -0.429038846   ', coefficients['pci_6moTRUE'],'
+  CABG last 6 months	      -0.661443369   ', coefficients['cabg_6moTRUE'],'
+  Previous/recurrent MI	    0.128214367    ', coefficients['hx_miTRUE'],'
+  Nitrates                  0.142262066    ', coefficients['long_nitrateTRUE'],'
   CVD RISK FACTORS
   SMOKING: Ex               0.104560407    ', coefficients['smokstatusEx'],'
   SMOKING: Current          0.28           ', coefficients['smokstatusCurrent'],'
+  SMOKING: missing                         ', coefficients['most_deprived_missingTRUE'],'
   Hypertension              -0.035521708   ', coefficients['hypertensionTRUE'],'
   Diabetes                  0.185590982    ', coefficients['diabetes_logicalTRUE'],'
   Total cholesterol         0.012691881    ', coefficients['total_chol_6mo_rescale'],'
+  Total cholesterol missing                ', coefficients['total_chol_6mo_rescale_missingTRUE'],'
   HDL, mmol/L               0.006510087    ', coefficients['hdl_6mo_rescale'],'
+  HDL, missing                             ', coefficients['hdl_6mo_rescale_missingTRUE'],'
   CVD COMORBIDITIES
   Heart failure             0.43416416     ', coefficients['heart_failureTRUE'],'
   PAD                       0.251746864    ', coefficients['padTRUE'],'
@@ -253,7 +289,22 @@ cat(
   Anxiety                   0.159257697    ', coefficients['hx_anxietyTRUE'],'
   BIOMARKERS	
   Heart rate, beats/min     0.093975676    ', coefficients['pulse_6mo_rescale'],'
+  Heart rate, missing                      ', coefficients['pulse_6mo_rescale_missingTRUE'],'
   Creatinine, mmol/L        0.063882169    ', coefficients['crea_6mo_rescale'],'
+  Creatinine, missing                      ', coefficients['crea_6mo_rescale_missingTRUE'],'
   White cell count, 109/L   0.113975087    ', coefficients['total_wbc_6mo_rescale'],'
-  Haemoglobin, g/dL         -0.26734457    ', coefficients['haemoglobin_6mo_rescale']
+  White cell count, missing                ', coefficients['total_wbc_6mo_rescale_missingTRUE'],'
+  Haemoglobin, g/dL         -0.26734457    ', coefficients['haemoglobin_6mo_rescale'],'
+  Haemoglobin, missing                     ', coefficients['haemoglobin_6mo_rescale_missingTRUE'],
+  '\n\n'
 )
+
+# Calculate C-indices on training and test sets
+# Get C-indices for training and test sets
+c.index.train <-
+  cIndex(fit.exp, COHORT.scaled[-test.set, ], model.type = 'cph')
+c.index.test <- 
+  cIndex(fit.exp, COHORT.scaled[test.set, ], model.type = 'cph')
+
+cat('C-index on training set:', c.index.train, '\n')
+cat('C-index on test set:', c.index.test)
