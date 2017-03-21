d0e2c72e38b8c2c49139d37bb0c5bbcdb9de8210
Andrew Steele <github@andrewsteele.co.uk>
2017-03-21 16:29:21 +0000

    Exploratory playing around with the data

---
+++ exploration/explore-dataset.R
@@ -0,0 +1,70 @@
+#+ knitr_setup, include = FALSE
+
+# Whether to cache the intensive code sections. Set to FALSE to recalculate
+# everything afresh.
+cacheoption <- FALSE
+# Disable lazy caching globally, because it fails for large objects, and all the
+# objects we wish to cache are large...
+opts_chunk$set(cache.lazy = FALSE)
+
+#' # Simple data investigations
+#' 
+
+data.filename <- '../../data/cohort-sanitised.csv'
+
+source('shared.R')
+
+COHORT.full <- data.frame(fread(data.filename))
+
+COHORT.use <- subset(COHORT.full, !exclude)
+
+#' ## Missing data
+#' 
+#' How much is there, and where is it concentrated?
+#' 
+#+ missing_data_plot
+
+interesting.vars <- 
+  c(
+    'age', 'gender', 'diagnosis', 'pci_6mo', 'cabg_6mo',
+    'hx_mi', 'long_nitrate', 'smokstatus', 'hypertension', 'diabetes',
+    'total_chol_6mo', 'hdl_6mo', 'heart_failure', 'pad', 'hx_af', 'hx_stroke',
+    'hx_renal', 'hx_copd', 'hx_cancer', 'hx_liver', 'hx_depression',
+    'hx_anxiety', 'pulse_6mo', 'crea_6mo', 'total_wbc_6mo','haemoglobin_6mo',
+    'imd_score'
+  )
+
+missingness <- 
+  unlist(lapply(COHORT.use[, interesting.vars], function(x){sum(is.na(x))}))
+
+missingness <- data.frame(var = names(missingness), n.missing = missingness)
+
+missingness$pc.missing <- missingness$n.missing / nrow(COHORT.use)
+
+ggplot(subset(missingness, n.missing > 0), aes(x = var, y = pc.missing)) +
+  geom_bar(stat = 'identity') +
+  ggtitle('% missingness by variable')
+
+#' ## Data distributions
+#' 
+#' How are the data distributed?
+#' 
+#+ data_distributions
+
+ggplot(COHORT.full) +
+  geom_histogram(aes(x = crea_6mo, fill = crea_6mo %% 10 != 0), binwidth = 1) +
+  xlim(0, 300)
+
+#' Creatinine levels are fairly smoothly distributed. The highlighted bins
+#' indicate numerical values divisible by 10, and there seems to be no
+#' particular bias. The small cluster of extremely low values could be
+#' misrecorded somehow.
+
+ggplot(COHORT.full) +
+  geom_histogram(aes(x = pulse_6mo, fill = pulse_6mo %% 10 != 0), binwidth = 1)+
+  xlim(0, 150)
+
+#' Heart rate data have high missingness, and those few values we do have are
+#' very heavily biased towards even numbers. The two most common values by some
+#' margin ae 60 and 80, though strangely 50, 70 and 90 are not similarly
+#' exceptional. There are also significant excesses at 100 and especially 120!
