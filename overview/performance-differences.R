e3d27d8357593310d488d161c2f7dfa59e6f0bde
Andrew Steele <github@andrewsteele.co.uk>
2017-08-24 18:37:13 +0100

    Allow more bootstrap formats

--- overview/performance-differences.R
+++ overview/performance-differences.R
@@ -4,9 +4,28 @@ bootstrap.base <- '../../output'
 
 bootstrap.files <-
   c(
-    cox.miss = 'caliber-replicate-with-missing-survreg-2-model-bootstrap.rds',
-    cox.disc = 'all-cv-survreg-boot-try5-surv-model.rds'
+    cox.miss = 'caliber-replicate-with-missing-survreg-6-linear-age-surv-boot.rds',
+    cox.disc = 'all-cv-survreg-boot-try5-surv-model.rds',
+    cox.imp = 'caliber-replicate-imputed-survreg-4-surv-boot-imp.rds',
+    rf = 'rfsrc-cv-nsplit-try3-surv-model-bootstraps.rds'
   )
+
+# Helper functions
+
+# Turn a boot object into a data frame
+bootstrap2Df <- function(x) {
+  df <- data.frame(x$t)
+  names(df) <- names(x$t0)
+  df
+}
+
+# Make sure calibration scores are bigger = better
+calibrationFix <- function(x) {
+  if(mean(x) < 0.5) {
+    x <- 1 - x
+  }
+  x
+}
 
 n <- length(bootstrap.files)
 
@@ -14,6 +33,16 @@ bootstraps <- list()
 
 for(i in 1:n) {
   bootstraps[[i]] <- readRDS(file.path(bootstrap.base, bootstrap.files[i]))
+  
+  if(class(bootstraps[[i]]) == 'list') {
+    # If it's a list, then it's from an imputed dataset with separate bootstraps
+    # Turn each of these into a data frame and then combine them together.
+    # (data.frame is needed because rbindlist returns a data.table)
+    bootstraps[[i]] <-
+      data.frame(rbindlist(lapply(bootstraps[[i]], bootstrap2Df)))
+  } else {
+    bootstraps[[i]] <- bootstrap2Df(bootstraps[[i]] )
+  }
 }
 
 x1x2 <- combn(1:n, 2)
@@ -24,14 +53,14 @@ x2 <- x1x2[2,]
 bootstrap.differences <- data.frame()
 for(i in 1:length(x1)) {
   # C-index
-  row.1.c.index <-
-    which(names(bootstraps[[x1[i]]]$t0) %in% c('c.test', 'c.index'))
-  row.2.c.index <-
-    which(names(bootstraps[[x2[i]]]$t0) %in% c('c.test', 'c.index'))
+  col.1.c.index <-
+    which(names(bootstraps[[x1[i]]]) %in% c('c.test', 'c.index'))
+  col.2.c.index <-
+    which(names(bootstraps[[x2[i]]]) %in% c('c.test', 'c.index'))
   boot.diff <-
     bootstrapDiff(
-      bootstraps[[x1[i]]]$t[, row.1.c.index],
-      bootstraps[[x2[i]]]$t[, row.2.c.index]
+      bootstraps[[x1[i]]][, col.1.c.index],
+      bootstraps[[x2[i]]][, col.2.c.index]
     )
   
   bootstrap.differences <-
@@ -48,12 +77,14 @@ for(i in 1:length(x1)) {
     )
   
   # Calibration score
-  row.1.calib <- which(names(bootstraps[[x1[i]]]$t0) == 'calibration.score')
-  row.2.calib <- which(names(bootstraps[[x2[i]]]$t0) == 'calibration.score')
+  col.1.calib <-
+    which(names(bootstraps[[x1[i]]]) == 'calibration.score')
+  col.2.calib <-
+    which(names(bootstraps[[x2[i]]]) == 'calibration.score')
   boot.diff <-
     bootstrapDiff(
-      bootstraps[[x1[i]]]$t[, row.1.calib],
-      bootstraps[[x2[i]]]$t[, row.1.calib]
+      calibrationFix(bootstraps[[x1[i]]][, col.1.calib]),
+      calibrationFix(bootstraps[[x2[i]]][, col.2.calib])
     )
   
   bootstrap.differences <-
@@ -72,3 +103,7 @@ for(i in 1:length(x1)) {
 
 # Remove nonsense row names
 rownames(bootstrap.differences) <- NULL
+
+print(bootstrap.differences)
+
+write.csv(bootstrap.differences, '../../output/bootstrap-differences.csv')
